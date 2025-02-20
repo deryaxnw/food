a09973132fc89d79c03d54965e705060f0e50862
Jao Ryan <joaoryanoliveira2005@gmail.com>
2025-02-20 17:53:27 -0300

    feat : produtos lançados

--- src/app/[slug]/menu/components/products.tsx
+++ src/app/[slug]/menu/components/products.tsx
@@ -1,4 +1,5 @@
 import { Product } from "@prisma/client";
+import Image from "next/image";
 import Link from "next/link";
 
 interface ProductsProps {
@@ -8,11 +9,23 @@ interface ProductsProps {
 
 const Products = ({products}: ProductsProps) => {
     return ( 
-        <div className="space-y-3">
+        <div className="space-y-3 px-5">
             {products.map(product => (
                 <Link key={product.id}  href='/' className="flex items-center justify-between gap-10 py-3">
                     <div>
                         <h3 className="text-sm font-medium">{product.name}</h3>
+                        <p className="p line-clamp-2 text-sm text-muted-foreground">
+                            {product.description}
+                        </p>
+                        <p className="pt-3 text-sm font-semibold">{new Intl.NumberFormat("pt-Br", {
+                            style: "currency",
+                            currency: "BRL",
+                        }).format(product.price)}</p>
+                    </div>
+
+
+                    <div className="relative min-h-[82px] min-w-[120px]">
+                        <Image src={product.imageUrl} alt={product.name} fill className="rounded-lg object-contain"/>
                     </div>
                 </Link>
             ))}
