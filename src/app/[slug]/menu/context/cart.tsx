6e398d3a34859e46fe768d559d9d1b8582196b31
Jao Ryan <joaoryanoliveira2005@gmail.com>
2025-02-23 20:08:01 -0300

    feat: add create contex for cart products

---
+++ src/app/[slug]/menu/context/cart.tsx
@@ -0,0 +1,50 @@
+"use client"
+
+
+
+import { Product } from "@prisma/client";
+import { createContext, ReactNode, useState } from "react";
+
+
+interface CartProducts extends Product {
+    quantity: number;
+}
+
+
+
+
+
+export interface ICartContext {
+    isOpen: boolean;
+    products: CartProducts[];
+    toggleCart: () => void;
+}
+
+
+export const CartContext = createContext<ICartContext>({
+    isOpen: false,
+    products: [],
+    toggleCart: () => {},
+})
+
+
+
+export const CartProvider = ({children} : {children: ReactNode }) => {
+    const [products, setProduct] = useState<CartProducts[]>([])
+    const [isOpen, setIsOpen] = useState<boolean>(false)
+
+    const toggleCart = () => {
+        setIsOpen(prev => !prev)
+    }
+    return (
+        <CartContext.Provider value={{
+            isOpen: isOpen,
+            products: products,
+            toggleCart: toggleCart,
+        }}>
+            {children}
+        </CartContext.Provider>
+    )
+}
+
+
