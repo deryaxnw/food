560724d49b4954752e3835a123733654ebda6cb6
Jao Ryan <joaoryanoliveira2005@gmail.com>
2025-02-23 20:08:45 -0300

    feat : sheet right product page

---
+++ src/app/[slug]/menu/components/cart-sheet.tsx
@@ -0,0 +1,25 @@
+import { useContext } from "react";
+
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from "@/components/ui/sheet";
+
+import { CartContext } from "../context/cart";
+
+const CartSheet = () => {
+    const {isOpen, toggleCart } = useContext(CartContext)
+
+
+
+    return ( <>
+      <Sheet open={isOpen} onOpenChange={toggleCart}>
+        <SheetContent>
+            <SheetHeader>
+                <SheetTitle>title</SheetTitle>
+                <SheetDescription>desck</SheetDescription>
+            </SheetHeader>
+        </SheetContent>
+    </Sheet>
+    
+    </> );
+}
+ 
+export default CartSheet;
