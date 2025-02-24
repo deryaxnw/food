a41c1ceb95c6315dce63c37fcd79af468bec9df3
Jao Ryan <joaoryanoliveira2005@gmail.com>
2025-02-24 08:48:06 -0300

    feat: add initial cart item component

--- src/app/[slug]/menu/components/cart-sheet.tsx
+++ src/app/[slug]/menu/components/cart-sheet.tsx
@@ -1,8 +1,9 @@
 import { useContext } from "react";
 
-import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetHeader, SheetTitle } from "@/components/ui/sheet";
 
 import { CartContext } from "../context/cart";
+import CartProductItem from "./cart-product-item";
 
 const CartSheet = () => {
     const {isOpen, toggleCart, products } = useContext(CartContext)
@@ -11,14 +12,15 @@ const CartSheet = () => {
 
     return ( <>
       <Sheet open={isOpen} onOpenChange={toggleCart}>
-        <SheetContent>
+        <SheetContent className="w-[80%]">
             <SheetHeader>
-                <SheetTitle>title</SheetTitle>
-                <SheetDescription>desck</SheetDescription>
+                <SheetTitle className="text-left">Sacola</SheetTitle>
             </SheetHeader>
+            <div className="py-5">
             {products.map(product => (
-                <h1 key={product.id}>{product.name} - {product.quantity}</h1>
+                <CartProductItem key={product.id} product={product}/>
             ))}
+            </div>
         </SheetContent>
     </Sheet>
     
--- src/app/[slug]/menu/context/cart.tsx
+++ src/app/[slug]/menu/context/cart.tsx
@@ -6,7 +6,7 @@ import { Product } from "@prisma/client";
 import { createContext, ReactNode, useState } from "react";
 
 
-interface CartProducts extends Pick<Product, 'name' | 'id' | 'price' | 'imageUrl'> {
+export interface CartProduct extends Pick<Product, 'name' | 'id' | 'price' | 'imageUrl'> {
     quantity: number;
 }
 
@@ -16,9 +16,9 @@ interface CartProducts extends Pick<Product, 'name' | 'id' | 'price' | 'imageUrl
 
 export interface ICartContext {
     isOpen: boolean;
-    products: CartProducts[];
+    products: CartProduct[];
     toggleCart: () => void;
-    addProduct: (product: CartProducts) => void
+    addProduct: (product: CartProduct) => void
 }
 
 
@@ -32,14 +32,14 @@ export const CartContext = createContext<ICartContext>({
 
 
 export const CartProvider = ({children} : {children: ReactNode }) => {
-    const [products, setProduct] = useState<CartProducts[]>([])
+    const [products, setProduct] = useState<CartProduct[]>([])
     const [isOpen, setIsOpen] = useState<boolean>(false)
 
     const toggleCart = () => {
         setIsOpen(prev => !prev)
     }
 
-    const addProduct = (product: CartProducts) => {
+    const addProduct = (product: CartProduct) => {
 
         const productIsAlreadyOnTheCart = products.some(prevProducts => prevProducts.id === product.id)
 
