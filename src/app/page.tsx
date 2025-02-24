0ec2840787e10f7f50c2aafbad594a6f1b2e7c9e
Jao Ryan <joaoryanoliveira2005@gmail.com>
2025-02-24 08:48:15 -0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+// import RestaurantPage from "./[slug]/page";
+
 const HomePage = () => {
-  return ( <h1>hello world</h1> );
+  return ( <>
+  
+<h1>oi</h1>
+  </>);
 }
  
 export default HomePage;
